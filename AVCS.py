a977740b6c3387b5fef6077e943a56fb88749ec6
Sonk <sls.slp36@gmail.com>
2015-11-09 21:53:08 +0700

    write data.csv

--- AVCS.py
+++ AVCS.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 from copy import deepcopy
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
 
 class AVCS:
     """docstring for ClassName"""
@@ -9,7 +11,7 @@ class AVCS:
         self.count = [0] * 2
         self.video = None
         self.fgMask = None
-        self.subtractor = cv2.BackgroundSubtractorMOG(history=500, nmixtures=10, backgroundRatio=0.5, noiseSigma=20)
+        self.subtractor = cv2.BackgroundSubtractorMOG2(500, 120, False)
         self.lane = {"0": {"upLeft": (0, 0), "upRight": (0, 0),
                        "lowLeft": (0, 0), "lowRight": (0, 0),
                        "is_empty": True, "pts": []},
@@ -52,11 +54,18 @@ class AVCS:
             cv2.imshow('frame', frame)
             cv2.waitKey(0)
 
+    def writeClusters(self, data, members):
+        dataF = open('data.csv', 'a')
+        for cluster in range(3):
+            for atr1, atr2 in data[members[cluster]]:
+                dataF.write(str(atr1) + ", " + str(atr2) + ", " + str(cluster) + "\n")
+        dataF.close()
+
     def run(self, cntStatus = True, saveVid = False, showVid = True ):
         self.video.set(cv2.cv.CV_CAP_PROP_POS_MSEC, 0)
         kernel = np.ones((10, 10), np.uint8)
         fourcc = cv2.cv.CV_FOURCC(*'XVID')
-        testW = cv2.VideoWriter('/home/sayong/videos.avi',fourcc,15,(640, 480))
+        testW = cv2.VideoWriter('videos.avi',fourcc,15,(640, 480))
         while self.video.isOpened():
             ret, frame = self.video.read()
             if not ret:
@@ -66,15 +75,19 @@ class AVCS:
             cv2.polylines(frame, [self.points[1]], True, (125, 0, 255), 3)
             filteredFrame = cv2.GaussianBlur(frame, (5, 5), 0)
             if self.fgMask is None:
-                self.fgMask = self.subtractor.apply(filteredFrame, 0.1)           
-            self.fgMask = self.subtractor.apply(filteredFrame, self.fgMask, 0.1)
-            self.fgMask = cv2.dilate(self.fgMask, kernel, iterations = 3)
-            self.fgMask = cv2.erode(self.fgMask, kernel, iterations = 1)
+                self.fgMask = self.subtractor.apply(filteredFrame, 0.0005)
+            self.fgMask = self.subtractor.apply(filteredFrame, self.fgMask, 0.001)
+            self.fgMask = cv2.dilate(self.fgMask, kernel, iterations=1)
+            self.fgMask = cv2.erode(self.fgMask, kernel, iterations=1)
+            self.fgMask = cv2.morphologyEx(self.fgMask, cv2.MORPH_CLOSE, np.ones((30, 30), np.uint8))
+            self.fgMask = cv2.morphologyEx(self.fgMask, cv2.MORPH_CLOSE, np.ones((30, 30), np.uint8))
             tempMask = deepcopy(self.fgMask)
             contours, hrc = cv2.findContours(tempMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             isIn = [False]*2
             for obj in contours:
                 moment = cv2.moments(obj)
+                if moment['m00'] == 0:
+                    continue
                 cx = int(moment['m10']/moment['m00'])
                 cy = int(moment['m01']/moment['m00'])
                 cv2.circle(res, (cx, cy), 3, (0, 0, 255), 4)
@@ -90,7 +103,7 @@ class AVCS:
                             self.lane[str(i)]["is_empty"] = False
                             self.lane[str(i)]["pts"].append((cx, cy))
                             self.count[i] += 1
-                            self.sizeCar[i].append(w*h )
+                            self.sizeCar[i].append( [w/float(h), w*h/cv2.contourArea(self.laneContour[i][0]) ] )
                             if w*h < 1600:
                                 self.typeCar["small"] += 1
                             elif w*h < 9500:
@@ -98,7 +111,7 @@ class AVCS:
                             else:
                                 self.typeCar["large"] += 1
 
-                        else: 
+                        else:
                             self.lane[str(i)]["pts"].insert(0, (cx, cy))
                         break
                     else:
@@ -127,29 +140,31 @@ class AVCS:
         testW.release()
         cv2.destroyAllWindows()
         print self.typeCar
-        # plt.figure(1)
-        # plt.plot(self.sizeCar[0],self.sizeCar[0], 'r*',self.sizeCar[1], self.sizeCar[1], 'rs')
-        # plt.show()
-        z = np.hstack((np.array(self.sizeCar[0]), np.array(self.sizeCar[1])))
-        z = z.reshape((z.size,1))
-        print 'test'
-        z = np.float32(z)
 
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        totalAtr = np.array(self.sizeCar[0]+ self.sizeCar[1])
+        k_means = KMeans(init='k-means++', n_clusters=3, n_init=10)
+        k_means.fit(totalAtr)
+        k_means_labels = k_means.labels_
+        k_means_cluster_centers = k_means.cluster_centers_
+        k_means_labels_unique = np.unique(k_means_labels)
+        colors = ['#4EACC5', '#FF9C34', '#4E9A06']
+        plt.figure()
 
-        # Set flags (Just to avoid line break in the code)
-        flags = cv2.KMEANS_RANDOM_CENTERS
-
-        # Apply KMeans
-        compactness,labels,centers = cv2.kmeans(z,3,criteria,10,flags)
-
-        #plt.hist(z,256,[0,256]),plt.show()
-        A = z[labels==0]
-        B = z[labels==1]
-        C = z[labels==2]
-        plt.hist(A,256,None,edgecolor = 'r')
-        plt.hist(B,256,None,edgecolor = 'b')
-        plt.hist(C,256,None,edgecolor = 'g')
-        plt.hist(centers,32,None,color = 'y')
+        plt.hold(True)
+        for k, col in zip(range(3), colors):
+            members = k_means_labels == k
+            cluster_center = k_means_cluster_centers[k]
+            plt.plot(totalAtr[members,0], totalAtr[members, 1], 'w',
+                    markerfacecolor=col, marker='.')
+            plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+                    markeredgecolor='k', markersize=6)
+        plt.title('KMeans')
+        plt.grid(True)
         plt.show()
-        import ipdb; ipdb.set_trace()
+        members = []
+        for cluster in range(3):
+            member = k_means_labels == cluster
+            members.append(member)
+        self.writeClusters(totalAtr, members)
+        import ipdb;ipdb.set_trace()
+
